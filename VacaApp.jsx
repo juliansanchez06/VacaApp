0e49393cc4968bb400929d6def6948aa29347fa7
juliansanchez06 <juliansanchez06@gmail.com>
2026-03-14 20:49:12 -0300

    VacaApp.jsx

--- VacaApp.jsx
+++ VacaApp.jsx
@@ -3103,7 +3103,6 @@ function Resultado({ R, s, rc, fl, gastos_calc, recria_margen, feedlot_margen, f
                   padding:"7px 0", borderBottom:`1px solid ${C.border}` }}>
                  <span style={{ fontSize: 11, color: C.t3 }}>{r.l}</span>
                   <span style={{ fontSize: 12, fontWeight: 700, color: r.c, fontFamily: "'DM Mono', monospace" }}>
-                  </div>
               ))}
             </div>
           </div>
