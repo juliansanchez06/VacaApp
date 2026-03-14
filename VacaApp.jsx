d4fca167478051da135394c4ae072abe43db81ae
juliansanchez06 <juliansanchez06@gmail.com>
2026-03-14 20:50:53 -0300

    VacaApp.jsx

--- VacaApp.jsx
+++ VacaApp.jsx
@@ -3098,13 +3098,7 @@ function Resultado({ R, s, rc, fl, gastos_calc, recria_margen, feedlot_margen, f
                 { l:"Ingresos recría", v: (rc.stock_cab||0)*(rc.stock_peso_actual||260)*(rc.stock_precio_venta||5500), c:C.amber },
                 { l:"Ingresos pastaje",v: pastaje_ing,  c:C.teal   },
                 { l:"Gastos campo",    v: gastos_tot,   c:C.red, neg:true },
-              ].map((r,i)=>(
-                <div key={i} style={{ display:"flex", justifyContent:"space-between",
-                  padding:"7px 0", borderBottom:`1px solid ${C.border}` }}>
-                 <span style={{ fontSize: 11, color: C.t3 }}>{r.l}</span>
-                  <span style={{ fontSize: 12, fontWeight: 700, color: r.c, fontFamily: "'DM Mono', monospace" }}>
-              ))}
-            </div>
+              </div>
           </div>
         );
       }
