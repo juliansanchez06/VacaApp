51ba5b00e88599738c6be38e2a73bc44b58a1e7b
juliansanchez06 <juliansanchez06@gmail.com>
2026-03-14 19:22:21 -0300

    main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import VacaApp from './VacaApp.jsx'
+import VacaApp from './VacaApp.jsx' // Agregale el .jsx acá por las dudas
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <VacaApp />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
