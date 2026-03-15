a4d9ebf89b9286dc71cc87a56714b5f0893e3ac6
juliansanchez06 <juliansanchez06@gmail.com>
2026-03-15 13:33:15 -0300

    VacaApp.jsx

--- VacaApp.jsx
+++ VacaApp.jsx
@@ -3112,7 +3112,7 @@ function Resultado({ R, s, rc, fl, gastos_calc, recria_margen, feedlot_margen, f
                 ))}
            </div>
         )
-    };
+    );
 
         
 export default VacaApp;
