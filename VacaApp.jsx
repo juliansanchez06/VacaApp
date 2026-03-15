06da918f207fe89927f602c136cd466612f00528
juliansanchez06 <juliansanchez06@gmail.com>
2026-03-15 13:16:45 -0300

    VacaApp.jsx

--- VacaApp.jsx
+++ VacaApp.jsx
@@ -3087,7 +3087,7 @@ function Resultado({ R, s, rc, fl, gastos_calc, recria_margen, feedlot_margen, f
                 </ResponsiveContainer>
               </div>
             )}
-          {/* Breakdown ingresos / egresos */}
+         {/* Breakdown ingresos / egresos */}
             <div style={{ background:C.card, borderRadius:20, padding:"20px", border:`1.5px solid ${C.border}`, marginTop:20 }}>
               <div style={{ fontSize:11, fontWeight:700, color:C.t3, textTransform:"uppercase", letterSpacing:1, marginBottom:14 }}>Estructura año 1</div>
               {[
@@ -3105,7 +3105,8 @@ function Resultado({ R, s, rc, fl, gastos_calc, recria_margen, feedlot_margen, f
               ))}
             </div>
           </div>
-        )}
-      }
+        </div>
+      );
+    }
 
 export default VacaApp;
