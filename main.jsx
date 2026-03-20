31218b67fa6d7ac8fc10268e172089bd8f59bf6e
juliansanchez06 <juliansanchez06@gmail.com>
2026-03-20 13:06:34 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import VacaApp from './VacaApp.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <VacaApp />
-  </React.StrictMode>,
-)
